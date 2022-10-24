79140033e9da254666d697112a682c52ac49bbcf
Chris <chriskonnerth@gmx.de>
2022-10-24 16:28:32 +0200

    fixed documentation

--- extensions/vscode/localize-it/extension.js
+++ extensions/vscode/localize-it/extension.js
@@ -38,22 +38,21 @@ function activate(context) {
 		// Helper to write line by line 
 		const  writeLine = (line) => content.write(`${line}\n`);  	
 		writeLine('import \'package:localize_it_annotation/localize_it_annotation.dart\';');	
-		writeLine('///');
+		content.write('\n');
 		writeLine('@localize_it');
 		writeLine('class LocaleConfiguration {');
 		writeLine('  /// Expects a lanugage code in **lowercase**.');
 		writeLine('  ///');
 		writeLine('  /// Supports all the currently available `source_lang`');
-		writeLine('  ///');
 		writeLine('  /// on [DeepL](https://www.deepl.com/de/docs-api/translate-text/translate-text/).');
 		writeLine('  /// ');
 		writeLine('  /// Defaults to: \'de\' (German)');
  		writeLine('  static const String baseLanguageCode = \'de\';');
 		content.write('\n');
 		writeLine('  /// Expects language codes in **lowercase**.');
+		writeLine('  /// Should *not* contain `baseLanguageCode`.');
 		writeLine('  ///');
 		writeLine('  /// Supports all the currently available `target_lang`');
-		writeLine('  ///');
 		writeLine('  /// on [DeepL](https://www.deepl.com/de/docs-api/translate-text/translate-text/).');
 		writeLine('  /// ');
 		writeLine('  /// Defaults to: `[\'en\' (English), \'es\' (Spanish)]`');
@@ -62,17 +61,16 @@ function activate(context) {
 		writeLine('    \'es\',');
 		writeLine('  ];');
 		writeLine('  /// Providing a `deepLAuthKey` enables translation generation');
-		writeLine('  ///  via the [DeepL API](https://www.deepl.com/de/pro-api?cta=header-pro-api/).');
+		writeLine('  /// via the [DeepL API](https://www.deepl.com/de/pro-api?cta=header-pro-api/).');
 		writeLine('  /// If no key is provided (empty String), all *marked Strings* (end with `.tr`) in your project');
-		writeLine('  /// will get translated to `\'--missing translation--\'`');
-		writeLine('  static const String deepLAuthKey = \'5bcb1202-12d5-2e63-f8ca-b087c47cad1b:fx\';');
-		content.writeLine('\n');
+		writeLine('  /// will get translated to `\'--missing translation--\'.`');
+		writeLine('  static const String deepLAuthKey = \'\';');
+		content.write('\n');
 		writeLine('  /// Enabling `useGetX` generates `Map<String, Map<String,String>> translationKeys` which can');
 		writeLine('  /// simply be passed to `GetMaterialApp`.');
 		writeLine('  static const bool useGetX = true;');
-
 		writeLine('}');
-		content.end()
+		content.end(); 
 
 		// Write content fo file
 		fs.writeFileSync(configFilePath, content, 'utf8');
